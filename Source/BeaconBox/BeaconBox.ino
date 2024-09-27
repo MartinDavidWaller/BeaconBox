16c85ffffb64ffdb02d323c1b830873a9415cb89
MartinDavidWaller <Martin@The-Wallers.net>
2024-09-27 16:20:15 +0100

    Code changes

--- Source/BeaconBox/BeaconBox.ino
+++ Source/BeaconBox/BeaconBox.ino
@@ -11,11 +11,12 @@
 
 #include "BeaconBox.h"
 #include "Configuration.h"
+#include "Dump.h"
 #include "LEDChain.h"
 
 // Define the working data
 
-// ???????????
+struct Configuration configuration;               // Configuration object
 
 // This is the setup routine. It all starts here.
 
@@ -34,6 +35,72 @@ void setup() {
 
   ledChainSetUp();
   ledChainBlinkAll();
+
+  // Read the configuration data from the EEPROM
+
+  Serial.println("");
+  Serial.println("Read the Configuration Data");
+  bool checkSumOK = readConfiguration(&configuration);
+
+  Serial.println("");
+  hexDump((char *)&configuration,sizeof(Configuration));
+
+  if (true == checkSumOK)
+    Serial.println("... CheckSum OK");
+  else
+    Serial.println("... CheckSum NOT OK");
+
+  // If the checksum is not OK or if the version numbers
+  // do not match then we must start again!
+
+  if ((false == checkSumOK) || 
+      (configuration.MajorVersion != PROGRAM_VERSION_MAJOR) || 
+      (configuration.MinorVersion != PROGRAM_VERSION_MINOR) ||
+      (0 == digitalRead(BOOT_PIN))) {
+
+    // Here the versions don't match or the checksum is wrong or
+    // the boot pin is pressed.
+    
+    if (true == checkSumOK) {
+      Serial.println("");
+      Serial.println("Different verison numbers.");
+      Serial.print("...Found V");
+      Serial.print(configuration.MajorVersion);
+      Serial.print(".");
+      Serial.println(configuration.MinorVersion);
+      Serial.print("...Expected V");
+      Serial.print(PROGRAM_VERSION_MAJOR);
+      Serial.print(".");
+      Serial.println(PROGRAM_VERSION_MINOR);    
+      Serial.println("");        
+    }
+      
+    // Initialise the configuration data
+
+    memset(&configuration,0,sizeof(Configuration));
+    configuration.MajorVersion = PROGRAM_VERSION_MAJOR;
+    configuration.MinorVersion = PROGRAM_VERSION_MINOR;
+    configuration.Flags = CLEAR;
+    SET_LED_ENABLED((&configuration));
+    SET_SOUND_ENABLED((&configuration));
+    strcpy((char*)&configuration.Hostname[0],PROGRAM_NAME);
+    strcpy((char*)&configuration.TimeZone[0],DEFAULT_TIMEZONE);
+    //configuration.Volume = DEFAULT_VOLUME;
+    //configuration.MinutesBetweenAlerts = DEFAULT_MINUTES_BETWEEN_ALERTS;
+    //configuration.FriendCycleCount = DEFAULT_FRIEND_CYCLE_COUNT;
+
+    Serial.println("Writing new configuration");
+    Serial.println("");
+    hexDump((char *)&configuration,sizeof(Configuration));
+
+    // Write it out
+    
+    writeConfiguration(&configuration); 
+  }
+
+  // Dump the current configuration
+  
+  dumpConfiguration(&configuration);    
 }
 
 void loop() {
--- Source/BeaconBox/Configuration.h
+++ Source/BeaconBox/Configuration.h
@@ -11,7 +11,7 @@
 #define WIFI_SSID_SIZE 32
 #define WIFI_PASSWORD_SIZE 64
 //#define CALLSIGN_SIZE 32
-//#define TIMEZONE_SIZE 30
+#define TIMEZONE_SIZE 30
 
 #define IS_EEPROM_VALID(c) ((c->Flags & EEPROM_VALID) == EEPROM_VALID)
 #define CLEAR_EEPROM_VALID(c) (c->Flags = (ConfigurationFlags)((int)c->Flags & ~EEPROM_VALID))
@@ -43,7 +43,7 @@ struct Configuration
   char WiFi_SSID[WIFI_SSID_SIZE + 1];
   char WiFi_Password[WIFI_PASSWORD_SIZE + 1];
   //char Callsign[CALLSIGN_SIZE + 1];
-  //char TimeZone[TIMEZONE_SIZE + 1];
+  char TimeZone[TIMEZONE_SIZE + 1];
   //uint8_t Volume;
   //uint8_t MinutesBetweenAlerts;
   //uint8_t FriendCycleCount;
